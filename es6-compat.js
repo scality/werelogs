3f52da8a91d25cfff4ba0789aa263dd07f55f19a
David Pineau <david.pineau@scality.com>
2015-12-09 19:07:51 +0100

    chore(ES6): Fix Babel6/ES6 usage
     - Fix es6-compat.js: Add explicit paths to translate to ES5
     - Require es6-compat.js from index.js for non-babel code that may depend on
     werelogs.

--- es6-compat.js
+++ es6-compat.js
@@ -1,1 +1,6 @@
-require('babel-core/register');
+require('babel-core/register')({
+    // Please list all paths to ES6-syntaxed code here.
+    ignore: filename =>
+        !(filename.startsWith(__dirname + '/lib/')
+          || filename.startsWith(__dirname + '/test/')),
+});
